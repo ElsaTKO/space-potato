3c8ef28fc61b3df2d56acfd47011b0dfbd4cc3fa
Elsa <elsa.moluf@gmail.com>
2015-09-05 00:02:47 -0700

    Added startRadio function on doc ready.

---
+++ space-potato.js
@@ -0,0 +1,11 @@
+$(document).ready( function() {
+
+  startRadio();
+
+});
+
+function startRadio() {
+  var radio = document.getElementById("radio");
+  radio.currentTime = 3.5;
+  // radio.play();
+}
