942eb82fda66063418911b456a125e15d76e5b85
Elsa <elsa.moluf@gmail.com>
2015-09-06 14:43:42 -0700

    Added click handler for selecting character. Adds and removes classes.

--- space-potato.js
+++ space-potato.js
@@ -1,7 +1,5 @@
 $(document).ready( function() {
-
   startRadio();
-
 });
 
 function startRadio() {
@@ -9,3 +7,19 @@ function startRadio() {
   radio.currentTime = 3.5;
   // radio.play();
 }
+
+$(".char-img").click(function() {
+  var character = $(this);
+  var others = $(".char-img").not($(this));
+
+  others.removeClass("neutral");
+  character.removeClass("neutral");
+
+  others.addClass("unselected");
+  character.removeClass("unselected");
+
+  others.removeClass("selected");
+  character.addClass("selected");
+
+
+});
