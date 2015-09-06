eb3e11b2aed9c788674c82d4fa2676cfce923eb3
Elsa <elsa.moluf@gmail.com>
2015-09-06 15:13:42 -0700

    Added test audio for each char when selected. Spaaace and whales.

--- space-potato.js
+++ space-potato.js
@@ -1,10 +1,14 @@
 $(document).ready( function() {
-  startRadio();
+  playRadio();
 });
 
-function startRadio() {
+var spacecore = $("#spacecore");
+var factcore = $("#factcore");
+
+function playRadio() {
   var radio = document.getElementById("radio");
   radio.currentTime = 3.5;
+  radio.volume = 0.1;
   // radio.play();
 }
 
@@ -21,5 +25,22 @@ $(".char-img").click(function() {
   others.removeClass("selected");
   character.addClass("selected");
 
+  // prepare audio clips
+  // assemble arrays for each char
+  // swap out active array depending on selected
 
+});
+
+$(document).keypress(function() {
+  if (spacecore.hasClass("selected")) {
+    console.log("space roar");
+    var spaaace = document.getElementById("space9");
+    spaaace.currentTime = 0;
+    spaaace.play();
+  } else if (factcore.hasClass("selected")) {
+    console.log("fact roar");
+    var whales = document.getElementById("fact5");
+    whales.currentTime = 0;
+    whales.play();
+  }
 });
