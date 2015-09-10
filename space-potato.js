4f55f6e46fe4613ace82d8b9a39e07239a3e2092
Elsa <elsa.moluf@gmail.com>
2015-09-09 23:17:17 -0700

    Added space core timings and updated map to include mapping to both text and timings. Now thinking through how to reqrite playAudio functionality with jPlayer.

--- space-potato.js
+++ space-potato.js
@@ -115,10 +115,48 @@ function playQuote() {
   quote.play();
   console.log(quote_queue.length);
 }
+// play quote through jPlayer
+// set media dynamically?
+
+// $("#jquery_jplayer_1").jPlayer("setMedia", {mp3: URL_VARIABLE_??? }).jPlayer("play");
+
+// $("#trackSelect").change(function(e) {
+//      my_trackName.text($(this).val());
+//      my_jPlayer.jPlayer("setMedia", {
+//         mp3: $(this).find(':selected').attr("href")
+//      });
+//  });
+
+// Chell in the Rain version
+// -------------------------
+// $('#jpId').bind($.jPlayer.event.timeupdate, function(event) {
+//      $('#time').html($.jPlayer.convertTime(event.jPlayer.status.currentTime));
+//
+//      if (event.jPlayer.status.currentTime >= timings[currentTrigger] && nolyrics != true) {
+//          fireTrigger(currentTrigger);
+//          currentTrigger++;
+//      }
+//
+//
+//   });
+
+// my version WIP
+// --------------
+// incrementor = 0;
+// set jPlayer media url
+// play audio
+//
+// if (event.jPlayer.status.currentTime >= timings[incrementor]) {
+//   create p tag
+//   append text[incrementor] into p tag
+//   incrementor++
+// }
 
 function findText(quote) {
   var quote_id = quote.id; // "space1"
-  var words = AudioTextMap[quote_id]; // space1_text
+  var words = AudioToTextAndTimingMap[quote_id]; // space1_text
+  // AudioToTextAndTimingMap[quote_id].text // space1_text
+  // AudioToTextAndTimingMap[quote_id].timing // space1_timing
   return words;
 }
 
@@ -138,7 +176,7 @@ $(document).keypress(function() {
 
 
 
-// QUOTE TEXT
+// QUOTE TEXT & TIMINGS
 // space core
 var space1_text = [];
 space1_text[0] = "Dad! I'm in space!";
@@ -146,15 +184,34 @@ space1_text[1] = "\"I'm proud of you, son.\"";
 space1_text[2] = "Dad, are you space?";
 space1_text[3] = "\"Yes. Now we are a family again.\"";
 
+var space1_timing = [];
+space1_timing[0] = 0;
+space1_timing[1] = 1;
+space1_timing[2] = 2;
+space1_timing[3] = 3;
+
+
 var space2_text = [];
 space2_text[0] = "Orbit.";
 space2_text[1] = "Space orbit.";
 space2_text[2] = "In my space suit.";
 
+var space2_timing = [];
+space2_timing[0] = 0;
+space2_timing[1] = 1;
+space2_timing[2] = 2;
+
+
 var space3_text = [];
 space3_text[0] = "Come here, space.";
 space3_text[1] = "I have a secret for you.";
 space3_text[2] = "No, come closer.";
+
+var space3_timing = [];
+space3_timing[0] = 0;
+space3_timing[1] = 1;
+space3_timing[2] = 2;
+
 
 var space4_text = [];
 space4_text[0] = "Lady.";
@@ -165,12 +222,28 @@ space4_text[4] = "\"ACE\"!";
 space4_text[5] = "Space.";
 space4_text[6] = "Space.";
 
+var space4_timing = [];
+space4_timing[0] = 0;
+space4_timing[1] = 1;
+space4_timing[2] = 2;
+space4_timing[3] = 3;
+space4_timing[4] = 4;
+space4_timing[5] = 5;
+space4_timing[6] = 6;
+
 
 var space5_text = [];
 space5_text[0] = "Space. Space.";
 space5_text[1] = "Space. Space.";
 space5_text[2] = "Comets. Stars.";
 space5_text[3] = "Galaxies. Orion.";
+
+var space5_timing = [];
+space5_timing[0] = 0;
+space5_timing[1] = 1;
+space5_timing[2] = 2;
+space5_timing[3] = 3;
+
 
 var space6_text = [];
 space6_text[0] = "Space court.";
@@ -181,11 +254,28 @@ space6_text[4] = "Guilty.";
 space6_text[5] = "Of being in space.";
 space6_text[6] = "I'm in space.";
 
+var space6_timing = [];
+space6_timing[0] = 0;
+space6_timing[1] = 1;
+space6_timing[2] = 2;
+space6_timing[3] = 3;
+space6_timing[4] = 4;
+space6_timing[5] = 5;
+space6_timing[6] = 6;
+
+
 var space7_text = [];
 space7_text[0] = "Where am I?";
 space7_text[1] = "Guess.";
 space7_text[2] = "Guess. Guess. Guess.";
 space7_text[3] = "I'm in space.";
+
+var space7_timing = [];
+space7_timing[0] = 0;
+space7_timing[1] = 1;
+space7_timing[2] = 2;
+space7_timing[3] = 3;
+
 
 var space8_text = [];
 space8_text[0] = "There's a star.";
@@ -196,11 +286,28 @@ space8_text[4] = "Star.";
 space8_text[5] = "Star. Star.";
 space8_text[6] = "Star.";
 
+var space8_timing = [];
+space8_timing[0] = 0;
+space8_timing[1] = 1;
+space8_timing[2] = 2;
+space8_timing[3] = 3;
+space8_timing[4] = 4;
+space8_timing[5] = 5;
+space8_timing[6] = 6;
+
+
 var space9_text = [];
 space9_text[0] = "SPAAAAAAAAAA";
 space9_text[1] = "AAAAAAAAAAAA";
 space9_text[2] = "AAAAAAAAAAAA";
 space9_text[3] = "AAAAAAAAACE!";
+
+var space9_timing = [];
+space9_timing[0] = 0;
+space9_timing[1] = 1;
+space9_timing[2] = 2;
+space9_timing[3] = 3;
+
 
 // fact core
 var fact1_text = [];
@@ -232,23 +339,23 @@ fact9_text[0] = "At some point in their lives 1 in 6 children will be abducted b
 
 
 // MAPPING
-var AudioTextMap = {
-  space1: space1_text,
-  space2: space2_text,
-  "space3": space3_text,
-  "space4": space4_text,
-  "space5": space5_text,
-  "space6": space6_text,
-  "space7": space7_text,
-  "space8": space8_text,
-  "space9": space9_text,
-  "fact1": fact1_text,
-  "fact2": fact2_text,
-  "fact3": fact3_text,
-  "fact4": fact4_text,
-  "fact5": fact5_text,
-  "fact6": fact6_text,
-  "fact7": fact7_text,
-  "fact8": fact8_text,
-  "fact9": fact9_text
+var AudioToTextAndTimingMap = {
+  space1: { text: space1_text, timing: space1_timing },
+  space2: { text: space2_text, timing: space2_timing },
+  space3: { text: space3_text, timing: space3_timing },
+  space4: { text: space4_text, timing: space4_timing },
+  space5: { text: space5_text, timing: space5_timing },
+  space6: { text: space6_text, timing: space6_timing },
+  space7: { text: space7_text, timing: space7_timing },
+  space8: { text: space8_text, timing: space8_timing },
+  space9: { text: space9_text, timing: space9_timing },
+  fact1: fact1_text,
+  fact2: fact2_text,
+  fact3: fact3_text,
+  fact4: fact4_text,
+  fact5: fact5_text,
+  fact6: fact6_text,
+  fact7: fact7_text,
+  fact8: fact8_text,
+  fact9: fact9_text
 };
